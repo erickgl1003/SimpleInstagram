a3f5eb36b1fa714f8738a65ff3cc5fb6ff4d4870
Erick Gómez <61529930+erickgl1003@users.noreply.github.com>
2021-07-06 15:59:51 -0500

    Posting is implemented.

--- app/src/main/java/com/example/simpleinstagram/ParseApplication.java
+++ app/src/main/java/com/example/simpleinstagram/ParseApplication.java
@@ -10,6 +10,9 @@ public class ParseApplication extends Application{
     public void onCreate() {
         super.onCreate();
 
+        // Register your parse models
+        ParseObject.registerSubclass(Post.class);
+
         //ParseObject.registerSubclass(Post.class);
         Parse.initialize(new Parse.Configuration.Builder(this)
                 .applicationId("77sb2llikDksdwA3nL7OKPJdeYs0eGUpZ0qAr1DJ")

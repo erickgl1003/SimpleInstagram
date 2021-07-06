a6e5f1183bc3bdf4cb6ff78624d35e26747e5c83
Erick Gómez <61529930+erickgl1003@users.noreply.github.com>
2021-07-06 12:46:01 -0500

    Initial commit

---
+++ app/src/main/java/com/example/simpleinstagram/ParseApplication.java
@@ -0,0 +1,21 @@
+package com.example.simpleinstagram;
+
+import android.app.Application;
+
+import com.parse.Parse;
+import com.parse.ParseObject;
+
+public class ParseApplication extends Application{
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        //ParseObject.registerSubclass(Post.class);
+        Parse.initialize(new Parse.Configuration.Builder(this)
+                .applicationId("77sb2llikDksdwA3nL7OKPJdeYs0eGUpZ0qAr1DJ")
+                .clientKey("9nTJBeOfcMLMS5d93NUpLlaB4MUAshsjgxJC1D0W")
+                .server("https://parseapi.back4app.com")
+                .build()
+        );
+    }
+}

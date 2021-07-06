a32aa2c562c24eb061466d589bac545744f4e92f
Erick Gómez <61529930+erickgl1003@users.noreply.github.com>
2021-07-06 12:57:47 -0500

    User can click topbar icon to logout

--- app/src/main/java/com/example/simpleinstagram/MainActivity.java
+++ app/src/main/java/com/example/simpleinstagram/MainActivity.java
@@ -3,6 +3,10 @@ package com.example.simpleinstagram;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.parse.ParseUser;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,24 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    // Inflate the menu; this adds items to the action bar if it is present.
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        //Inflate the menu
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+        if(item.getItemId() == R.id.logout){
+            ParseUser.logOut();
+            finish();
+            // navigate backwards to Login screen
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
